2ee18d3d42fec0d600cf02f9cdc631ceb638ea76
Shoaib Bhimani <shoaibbhimani1392@gmail.com>
2021-12-26 01:38:17 +0530

    lesson_3

--- src/components/notes-list/index.test.tsx
+++ src/components/notes-list/index.test.tsx
@@ -2,7 +2,9 @@ import {
   render,
   screen,
   waitForElementToBeRemoved,
+  getByText,
 } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { rest } from "msw";
 import { setupServer } from "msw/node";
@@ -14,6 +16,17 @@ const server = setupServer();
 describe("Given NotesList", () => {
   beforeAll(() => {
     server.listen();
+  });
+
+  beforeEach(() => {
+    // IntersectionObserver isn't available in test environment
+    const mockIntersectionObserver = jest.fn();
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null,
+    });
+    window.IntersectionObserver = mockIntersectionObserver;
   });
 
   afterEach(() => {
@@ -32,27 +45,31 @@ describe("Given NotesList", () => {
 
   // Assertion
 
-  test("WHEN notes-list component is mounted THEN render list of notes", async () => {
-    server.use(
+  const customSetupServer = ({
+    notes = [
+      {
+        id: 0,
+        title: "Learn React",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in neque nisi. Phasellus placerat erat arcu. Fusce sed vehicula sem, vel viverra sapien. Quisque dapibus blandit ipsum nec aliquet. Suspendisse eget volutpat felis. Sed interdum turpis ac nulla imperdiet mollis. Mauris non iaculis enim. Nullam pretium metus purus, nec sagittis sapien tincidunt eget. Sed leo diam, sodales vel enim eu, vestibulum volutpat magna.",
+      },
+      {
+        id: 1,
+        title: "Learn React Query",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in neque nisi. Phasellus placerat erat arcu. Fusce sed vehicula sem, vel viverra sapien. Quisque dapibus blandit ipsum nec aliquet. Suspendisse eget volutpat felis. Sed interdum turpis ac nulla imperdiet mollis. Mauris non iaculis enim. Nullam pretium metus purus, nec sagittis sapien tincidunt eget. Sed leo diam, sodales vel enim eu, vestibulum volutpat magna.",
+      },
+    ],
+  } = {}) => {
+    return server.use(
       rest.get("http://localhost:4333/notes-list", (req, res, ctx) => {
         // Respond with a mocked user token that gets persisted
         // in the `sessionStorage` by the `Login` component.
-        return res(
-          ctx.json([
-            {
-              id: 0,
-              title: "Learn React",
-              desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in neque nisi. Phasellus placerat erat arcu. Fusce sed vehicula sem, vel viverra sapien. Quisque dapibus blandit ipsum nec aliquet. Suspendisse eget volutpat felis. Sed interdum turpis ac nulla imperdiet mollis. Mauris non iaculis enim. Nullam pretium metus purus, nec sagittis sapien tincidunt eget. Sed leo diam, sodales vel enim eu, vestibulum volutpat magna.",
-            },
-            {
-              id: 1,
-              title: "Learn React Query",
-              desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in neque nisi. Phasellus placerat erat arcu. Fusce sed vehicula sem, vel viverra sapien. Quisque dapibus blandit ipsum nec aliquet. Suspendisse eget volutpat felis. Sed interdum turpis ac nulla imperdiet mollis. Mauris non iaculis enim. Nullam pretium metus purus, nec sagittis sapien tincidunt eget. Sed leo diam, sodales vel enim eu, vestibulum volutpat magna.",
-            },
-          ])
-        );
+        return res(ctx.json(notes));
       })
     );
+  };
+
+  test("WHEN notes-list component is mounted THEN render list of notes", async () => {
+    customSetupServer();
 
     render(<NotesList />);
 
@@ -110,5 +127,70 @@ describe("Given NotesList", () => {
     const errorMsg = screen.getByText("Something wen't wrong");
 
     expect(errorMsg).toBeInTheDocument();
+  });
+
+  test(`
+  Scenario: User should be able to add note
+  WHEN we click on add note button THEN render new note modal
+  And WHEN we add title and description in add note form  and click on save THEN note should be added notes-list
+  `, async () => {
+    const notes = [
+      {
+        id: 0,
+        title: "Learn React",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in neque nisi. Phasellus placerat erat arcu. Fusce sed vehicula sem, vel viverra sapien. Quisque dapibus blandit ipsum nec aliquet. Suspendisse eget volutpat felis. Sed interdum turpis ac nulla imperdiet mollis. Mauris non iaculis enim. Nullam pretium metus purus, nec sagittis sapien tincidunt eget. Sed leo diam, sodales vel enim eu, vestibulum volutpat magna.",
+      },
+      {
+        id: 1,
+        title: "Learn React Query",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in neque nisi. Phasellus placerat erat arcu. Fusce sed vehicula sem, vel viverra sapien. Quisque dapibus blandit ipsum nec aliquet. Suspendisse eget volutpat felis. Sed interdum turpis ac nulla imperdiet mollis. Mauris non iaculis enim. Nullam pretium metus purus, nec sagittis sapien tincidunt eget. Sed leo diam, sodales vel enim eu, vestibulum volutpat magna.",
+      },
+    ];
+
+    customSetupServer({ notes: notes });
+
+    server.use(
+      rest.post("http://localhost:4333/notes-list", (req, res, ctx) => {
+        // Respond with a mocked user token that gets persisted
+        // in the `sessionStorage` by the `Login` component.
+
+        notes.push({
+          id: 2,
+          title: "Learn mobx",
+          desc: "State Management Solution",
+        });
+
+        return res(ctx.status(200));
+      })
+    );
+
+    render(<NotesList />);
+
+    // wait for Data to load
+    await waitForElementToBeRemoved(() =>
+      screen.getByText("Loading Data ....")
+    );
+
+    const addNoteButton = screen.getByText("Add Notes");
+
+    userEvent.click(addNoteButton);
+
+    const modal = screen.getByTestId("modal");
+
+    expect(modal).toBeInTheDocument();
+
+    userEvent.type(screen.getByTestId("title"), "Learn mobx");
+
+    userEvent.type(screen.getByTestId("desc"), "State Management Solution");
+
+    userEvent.click(screen.getByTestId("save"));
+
+    await waitForElementToBeRemoved(() => {
+      return screen.getByTestId("modal");
+    });
+
+    getByText(screen.getByTestId("note-list"), "Learn mobx");
+
+    expect(screen.queryByTestId("modal")).toBeNull();
   });
 });
--- src/components/notes-list/index.tsx
+++ src/components/notes-list/index.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+
+import { Modal } from "../../shared/modal";
 
 type note = {
   id: number;
@@ -12,8 +14,11 @@ type notes = note[];
 export const NotesList = () => {
   const [notes, setNotes] = useState<notes | null>(null);
   const [error, setError] = useState(false);
+  const [showAddNoteModal, setShowAddNoteModal] = useState(false);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
 
-  useEffect(() => {
+  const fetchNotes = () => {
     axios
       .get("http://localhost:4333/notes-list")
       .then((response) => {
@@ -23,6 +28,32 @@ export const NotesList = () => {
       .catch(() => {
         setError(true);
       });
+  };
+
+  const onAddNoteSubmit = (event: React.FormEvent) => {
+    console.log("onsubmit");
+
+    // refetch notes list
+    event.preventDefault();
+
+    // Make POst api call
+    axios
+      .post("http://localhost:4333/notes-list", {
+        title,
+        desc,
+      })
+      .then((response) => {
+        // Refetch Notes
+        fetchNotes();
+        setShowAddNoteModal(false);
+      })
+      .catch(() => {
+        setError(true);
+      });
+  };
+
+  useEffect(() => {
+    fetchNotes();
   }, []);
 
   if (error) {
@@ -36,15 +67,74 @@ export const NotesList = () => {
   return (
     <div>
       {notes.length > 0 ? (
-        <ul>
-          {notes.map((note) => {
-            return (
-              <li key={note.id} data-testid="notes-list-item">
-                {note.title}
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <ul data-testid="note-list">
+            {notes.map((note) => {
+              return (
+                <li key={note.id} data-testid="notes-list-item">
+                  {note.title}
+                </li>
+              );
+            })}
+          </ul>
+
+          <div>
+            <button onClick={() => setShowAddNoteModal(true)}>
+              Add Notes{" "}
+            </button>
+          </div>
+
+          <Modal
+            isOpen={showAddNoteModal}
+            closeModal={() => {
+              setShowAddNoteModal(false);
+            }}
+          >
+            <div>
+              <form onSubmit={onAddNoteSubmit}>
+                <div>
+                  <label htmlFor="title" className="flex">
+                    <div>Title</div>
+                    <input
+                      type="text"
+                      className="p-2 bg-gray-50 border ml-2 w-full"
+                      data-testid="title"
+                      value={title}
+                      onChange={(event) => {
+                        setTitle(event.target.value);
+                      }}
+                    />
+                  </label>
+                </div>
+
+                <div className="mt-2">
+                  <label htmlFor="desc" className="flex">
+                    <div>Desc</div>
+                    <textarea
+                      id="desc"
+                      data-testid="desc"
+                      className="p-2 bg-gray-50 border ml-2 w-full"
+                      value={desc}
+                      onChange={(event) => {
+                        setDesc(event.target.value);
+                      }}
+                    />
+                  </label>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    className="bg-purple-400  text-white rounded-sm py-2 px-4"
+                    type="submit"
+                    data-testid="save"
+                  >
+                    Save
+                  </button>
+                </div>
+              </form>
+            </div>
+          </Modal>
+        </div>
       ) : (
         <div> No Notes Avaliable </div>
       )}
